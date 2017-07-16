27eda6a2be86d8bbf114ce6ce190292a38845710
cnotv <contact@cnotv.xyz>
2017-07-16 15:54:40 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <main id="app" class="cnotv__main cnotv__container">
+    <a href="https://cnotv.xyz/" class="cnotv__logo-link" rel="home" itemprop="url">
+      <img src="./assets/logo.svg" class="cnotv__logo" alt="cnotv" itemprop="logo">
+    </a>
+    <router-view></router-view>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'app',
+};
+</script>
+
+<style lang="scss" src="./assets/scss/main.scss"></style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="cnotv__section">
+    <h1>{{ msg }}</h1>
+    <h3>{{ msg2 }}</h3>
+    <p>{{ msg3 }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data() {
+    return {
+      msg: 'Styleguide and framework for cnotv.',
+      msg2: 'Built with Vue, SCSS and KSS.',
+      msg3: '(Soon online with built automation using Travis.)',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.cnotv__section {
+  text-align: center;
+}
+</style>
