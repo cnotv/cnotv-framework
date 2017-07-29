74187553f3bba8ff989955e8743df1c733c1756a
cnotv <contact@cnotv.xyz>
2017-07-30 01:03:58 +0200

    updated script3

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 # deploy.sh
-set -e
 
-sudo apt-get install -y lftp
-
-# deployment via ftp upload. Using FTPS for that
-lftp -c "open -u $FTP_USER,$FTP_PASSWORD $FTP_HOST set ssl:verify-certificate no; mirror -R ~/home/travis/build/cnotv/cnotv-framework /framework.cnotv.xyz"
+"curl -T $FTP_USER:$FTP_PASSWORD $FTP_HOST"
