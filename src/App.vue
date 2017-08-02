c8ede3e2d4e8d8baf65d77ca21cfba36a4f5aed2
cnotv <contact@cnotv.xyz>
2017-08-02 22:16:21 +0200

    updated inventory

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
-  <div class="c-row">
+  <div class="c-row--edge">
     <aside class="c-sidebar">
       <nav class="c-nav-v" tabindex="-1">
         <img src="./assets/logo.svg" class="c-logo c-nav-v__toggle" alt="cnotv" itemprop="logo">
         <button class="c-nav-v__close"><i class="fa fa-close"></i></button>
+        <router-link to="/">Introduction</router-link>
         <ul>
           <li>
             <span>Common</span>
             <ul>
               <li><router-link to="/text">Text</router-link></li>
+              <li><router-link to="/mixin">Mixin</router-link></li>
+              <li><router-link to="/colors">Colors</router-link></li>
+              <li><router-link to="/icons">Icons</router-link></li>
             </ul>
           </li>
           <li>
@@ -17,26 +21,42 @@
               <li>
                 <router-link to="/layout">Layout</router-link>
                 <ul>
-                  <li><a href="#Grid">Grid</a></li>
+                  <li><a href="/layout#Grid">Grid</a></li>
+                  <li><a href="/layout#Section">Section</a></li>
+                  <li><a href="/layout#Container">Container</a></li>
                 </ul>
               </li>
-              <li><a href="#Inputs">Inputs</a></li>
-              <li><a href="#Inputs">Buttons</a></li>
+              <li><a href="/inputs">Inputs</a></li>
+              <li>
+                <a href="/buttons">Buttons</a>
+                <ul>
+                  <li><a href="/buttons#icons">Icons</a></li>
+                </ul>
+              </li>
+              <li><a href="/backgrounds">Backgrounds</a></li>
+              <li><a href="/loader">Loader</a></li>
+              <li><a href="/animations">Animations</a></li>
             </ul>
           </li>
           <li>
             <span>Objects</span>
             <ul>
-              <li><a href="#Header">Header</a></li>
-              <li><a href="#Footer">Footer</a></li>
               <li>
-                <a href="#Nav">Nav</a>
+                <a href="/nav">Nav</a>
                 <ul>
-                  <li><a href="#H">Horizontal</a></li>
-                  <li><a href="#V">Vertical</a></li>
+                  <li><a href="/nav#H">Horizontal</a></li>
+                  <li><a href="/nav#V">Vertical</a></li>
                 </ul>
               </li>
-              <li><a href="#Modal">Modal</a></li>
+              <li>
+                <a href="/cards">Cards</a>
+                <ul>
+                  <li><a href="/cards#portfolio">Portfolio</a></li>
+                  <li><a href="/cards#article">Article</a></li>
+                </ul>
+              </li>
+              <li><a href="/searchbar">Search bar</a></li>
+              <li><a href="/modal">Modal</a></li>
             </ul>
           </li>
         </ul>
--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -2,56 +2,73 @@
   <section class="c-section">
     <div class="c-container">
       <h1>Layout</h1>
-      <h2 id="Grid">1. Grid</h2>
-      <div class="c-row c-row--padding-2">
-        <div class="c-col">.c-col</div>
-      </div>
-      <div class="c-row c-row--padding-2">
-        <div class="c-col-1-2">.c-col-1-2</div>
-        <div class="c-col-1-2">.c-col-1-2</div>
-      </div>
-      <div class="c-row c-row--padding-2">
-        <div class="c-col-1-3">.c-col-1-3</div>
-        <div class="c-col-1-3">.c-col-1-3</div>
-        <div class="c-col-1-3">.c-col-1-3</div>
-      </div>
-      <div class="c-row c-row--padding-2">
-        <div class="c-col-1-4">.c-col-1-4</div>
-        <div class="c-col-1-4">.c-col-1-4</div>
-        <div class="c-col-1-4">.c-col-1-4</div>
-        <div class="c-col-1-4">.c-col-1-4</div>
-      </div>
-      <div class="c-row c-row--padding-2">
-        <div class="c-col-1-5">.c-col-1-5</div>
-        <div class="c-col-1-5">.c-col-1-5</div>
-        <div class="c-col-1-5">.c-col-1-5</div>
-        <div class="c-col-1-5">.c-col-1-5</div>
-        <div class="c-col-1-5">.c-col-1-5</div>
-      </div>
-      <div class="c-row c-row--padding-2">
-        <div class="c-col-1-6">.c-col-1-6</div>
-        <div class="c-col-1-6">.c-col-1-6</div>
-        <div class="c-col-1-6">.c-col-1-6</div>
-        <div class="c-col-1-6">.c-col-1-6</div>
-        <div class="c-col-1-6">.c-col-1-6</div>
-        <div class="c-col-1-6">.c-col-1-6</div>
-      </div>
-      <div class="c-row c-row--padding-2">
-        <div class="c-col-1-3">.c-col-1-3</div>
-        <div class="c-col-2-3">.c-col-2-3</div>
-      </div>
-      <div class="c-row c-row--padding-2">
-        <div class="c-col-1-4">.c-col-1-4</div>
-        <div class="c-col-3-4">.c-col-3-4</div>
-      </div>
-      <div class="c-row c-row--padding-2">
-        <div class="c-col-2-5">.c-col-2-5</div>
-        <div class="c-col-3-5">.c-col-3-5</div>
-      </div>
-      <div class="c-row c-row--padding-2">
-        <div class="c-col-1-6">.c-col-1-6</div>
-        <div class="c-col-5-6">.c-col-5-6</div>
-      </div>
+
+      <section class="c-section">
+        <h2 id="Grid">1. Grid</h2>
+        <div class="c-row c-row--padding-2">
+          <div class="c-col">.c-col</div>
+        </div>
+        <div class="c-row c-row--padding-2">
+          <div class="c-col-1-2">.c-col-1-2</div>
+          <div class="c-col-1-2">.c-col-1-2</div>
+        </div>
+        <div class="c-row c-row--padding-2">
+          <div class="c-col-1-3">.c-col-1-3</div>
+          <div class="c-col-1-3">.c-col-1-3</div>
+          <div class="c-col-1-3">.c-col-1-3</div>
+        </div>
+        <div class="c-row c-row--padding-2">
+          <div class="c-col-1-4">.c-col-1-4</div>
+          <div class="c-col-1-4">.c-col-1-4</div>
+          <div class="c-col-1-4">.c-col-1-4</div>
+          <div class="c-col-1-4">.c-col-1-4</div>
+        </div>
+        <div class="c-row c-row--padding-2">
+          <div class="c-col-1-5">.c-col-1-5</div>
+          <div class="c-col-1-5">.c-col-1-5</div>
+          <div class="c-col-1-5">.c-col-1-5</div>
+          <div class="c-col-1-5">.c-col-1-5</div>
+          <div class="c-col-1-5">.c-col-1-5</div>
+        </div>
+        <div class="c-row c-row--padding-2">
+          <div class="c-col-1-6">.c-col-1-6</div>
+          <div class="c-col-1-6">.c-col-1-6</div>
+          <div class="c-col-1-6">.c-col-1-6</div>
+          <div class="c-col-1-6">.c-col-1-6</div>
+          <div class="c-col-1-6">.c-col-1-6</div>
+          <div class="c-col-1-6">.c-col-1-6</div>
+        </div>
+        <div class="c-row c-row--padding-2">
+          <div class="c-col-1-3">.c-col-1-3</div>
+          <div class="c-col-2-3">.c-col-2-3</div>
+        </div>
+        <div class="c-row c-row--padding-2">
+          <div class="c-col-1-4">.c-col-1-4</div>
+          <div class="c-col-3-4">.c-col-3-4</div>
+        </div>
+        <div class="c-row c-row--padding-2">
+          <div class="c-col-2-5">.c-col-2-5</div>
+          <div class="c-col-3-5">.c-col-3-5</div>
+        </div>
+        <div class="c-row c-row--padding-2">
+          <div class="c-col-1-6">.c-col-1-6</div>
+          <div class="c-col-5-6">.c-col-5-6</div>
+        </div>
+      </section>
+
+      <section class="c-section">
+        <h2 id="Section">2. Section</h2>
+        <section class="c-section c-bg-lightgrey">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ante eros, euismod ac facilisis ac, rhoncus non metus. Curabitur vehicula pretium sem, quis feugiat massa varius ac. Nam aliquam libero eu magna fringilla posuere. Phasellus elementum varius dui quis lacinia. Aenean aliquet lorem a erat maximus condimentum. Vivamus nulla massa, scelerisque quis dolor id, sodales molestie mi. Integer ac dignissim tellus. Nam luctus sed nibh non aliquet. Quisque ornare lacinia augue id convallis. Praesent cursus nibh eu leo vehicula pellentesque. 
+        </section>
+      </section>
+
+      <section class="c-section">
+        <h2 id="Container">3. Container</h2>
+        <div class="c-container c-bg-lightgrey">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ante eros, euismod ac facilisis ac, rhoncus non metus. Curabitur vehicula pretium sem, quis feugiat massa varius ac. Nam aliquam libero eu magna fringilla posuere. Phasellus elementum varius dui quis lacinia. Aenean aliquet lorem a erat maximus condimentum. Vivamus nulla massa, scelerisque quis dolor id, sodales molestie mi. Integer ac dignissim tellus. Nam luctus sed nibh non aliquet. Quisque ornare lacinia augue id convallis. Praesent cursus nibh eu leo vehicula pellentesque. 
+        </div>
+      </section>
     </div>
   </section>
 </template>
